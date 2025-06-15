9d0bcdd4fa7e33449248975b029d7039753d1473
toobi-jpeg <rex.98@live.se>
2025-06-15 13:19:58 +0200

    final final

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,12 @@ import BootstrapIcon from "../../public/bootstrapIcon.jsx";
 import viteLogo from "../../public//vite.svg";
 import reactLogo from "../assets/react.svg";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Home() {
   const courses = CoursesData();
   const { registeredCourses } = useCourses();
+  const [hoveredIcon, setHoveredIcon] = useState(null);
   const hasRegisteredCourses =
     registeredCourses && registeredCourses.length > 0;
 
@@ -24,22 +26,51 @@ export default function Home() {
       <div className="mb-0">
         <h4 className="text-center custom-text-gradient">Made with</h4>
         <div className="d-flex flex-row gap-5 justify-content-center align-items-center mt-4">
-          <div className="customCardGradient p-3 custom-border rounded">
+          <div
+            className="position-relative customCardGradient p-3 custom-border rounded logo-hover"
+            onMouseEnter={() => setHoveredIcon("Vue")}
+            onMouseLeave={() => setHoveredIcon(null)}
+          >
             <img
               src={viteLogo}
               style={{ width: "4rem", height: "4rem" }}
               alt="Vite Logo"
+              className="logo-hover"
             />
+            {hoveredIcon === "Vue" && (
+              <p className="position-absolute mt-4 top-100 start-50 translate-middle fs-5">
+                {hoveredIcon}
+              </p>
+            )}
           </div>
-          <div className="customCardGradient p-3 custom-border rounded">
+          <div
+            className="position-relative customCardGradient p-3 custom-border rounded logo-hover"
+            onMouseEnter={() => setHoveredIcon("React")}
+            onMouseLeave={() => setHoveredIcon(null)}
+          >
             <img
               src={reactLogo}
               style={{ width: "4rem", height: "4rem" }}
               alt="React Logo"
+              className="logo-hover"
             />
+            {hoveredIcon === "React" && (
+              <p className="position-absolute mt-4 top-100 start-50 translate-middle fs-5">
+                {hoveredIcon}
+              </p>
+            )}
           </div>
-          <div className="customCardGradient p-2 custom-border rounded">
-            <BootstrapIcon width="5rem" height="5rem" />
+          <div
+            className="position-relative customCardGradient p-2 custom-border rounded logo-hover"
+            onMouseEnter={() => setHoveredIcon("Bootstrap")}
+            onMouseLeave={() => setHoveredIcon(null)}
+          >
+            <BootstrapIcon width="5rem" height="5rem" className="logo-hover" />
+            {hoveredIcon === "Bootstrap" && (
+              <p className="position-absolute mt-4 top-100 start-50 translate-middle fs-5">
+                {hoveredIcon}
+              </p>
+            )}
           </div>
         </div>
       </div>
